f6abeadd9f7ae68e0489a8cce40c757c6aa81b48
mete <avavrupa180@gmail.com>
2023-04-10 18:15:58 +0200

    adding more comments and readme file

---
+++ README.md
@@ -0,0 +1,34 @@
+Loan Calculator
+
+A simple graphical user interface (GUI) application using Tkinter for calculating loan payments. This program allows users to input the annual interest rate, number of years, and loan amount, then calculates the monthly payment and total payment for the loan.
+Table of Contents
+
+    Installation
+    Usage
+    Contributing
+    License
+    Acknowledgements
+
+Installation
+
+    Make sure you have Python 3.x installed on your system. If not, download and install it from the official website.
+    Clone this repository to your local machine.
+    Ensure that Tkinter is installed. It usually comes pre-installed with Python. If not, refer to the Tkinter installation guide.
+
+Usage
+
+    Open a terminal or command prompt, navigate to the directory where the script is located.
+    Run the script using python loan_calculator.py or python3 loan_calculator.py.
+    Input the annual interest rate, number of years, and loan amount into the corresponding fields.
+    Click on the "Compute Payment" button to calculate the monthly and total payment for the loan.
+    To clear the input fields and results, click on the "Clear" button.
+
+Contributing
+
+Contributions are welcome. Please open an issue or submit a pull request to suggest changes or improvements.
+License
+
+This project is licensed under the MIT License. See the LICENSE file for more information.
+Acknowledgements
+
+    Tkinter library for creating the GUI
--- loan_fee_gui_calculator.py
+++ loan_fee_gui_calculator.py
@@ -1,32 +1,26 @@
-# importing every object from tkinter module
-from cmath import e
-from textwrap import fill
 from tkinter import *
-from turtle import back
 
-# creating module
+# Creating the Loan Calculator class
 class App:
     def __init__(self):
-        # creating Tk windows
+        # Creating the Tkinter window
         root = Tk()
 
-        # specs of tk windows
+        # Setting window title and making it unresizable
         root.title("Loan Calculator")
-
-        # unreasizable windows
         root.resizable(height=False, width=False)
 
-        # setting back-ground color to windows
-        root.configure(background = "light green")        
+        # Setting background color for the window
+        root.configure(background="light green")
 
-        # creating labels
+        # Creating labels for input fields
         Label(root, font="Helvetica 12 bold", bg="light green", text="Annual Interest Rate").grid(row=1, column=1, sticky=W)
-        Label(root, font="Helvetica 12 bold", bg="light green", text="NUmber of Years").grid(row=2, column=1, sticky=W)
+        Label(root, font="Helvetica 12 bold", bg="light green", text="Number of Years").grid(row=2, column=1, sticky=W)
         Label(root, font="Helvetica 12 bold", bg="light green", text="Loan Amount").grid(row=3, column=1, sticky=W)
         Label(root, font="Helvetica 12 bold", bg="light green", text="Monthly Payment").grid(row=4, column=1, sticky=W)
         Label(root, font="Helvetica 12 bold", bg="light green", text="Total Payment").grid(row=5, column=1, sticky=W)
 
-        # accepting inputs for labels from user
+        # Creating input fields for the labels
         self.annualInterestRateVar = StringVar()
         Entry(root, textvariable=self.annualInterestRateVar, justify=RIGHT).grid(row=1, column=2)
         self.numberofYearsVar = StringVar()
@@ -34,52 +28,53 @@ class App:
         self.loanAmountVar = StringVar()
         Entry(root, textvariable=self.loanAmountVar, justify=RIGHT).grid(row=3, column=2)
 
-        # labels for montly and total payment
+        # Labels for displaying monthly and total payment
         self.monthlyPaymentVar = StringVar()
         lblMonthlyPayment = Label(root, textvariable=self.monthlyPaymentVar).grid(row=4, column=2, sticky=E)
 
         self.totalPaymentVar = StringVar()
         lblTotalPaymentVar = Label(root, textvariable=self.totalPaymentVar).grid(row=5, column=2, sticky=E)
 
-        # compute payment
+        # Compute Payment button
         btComputePayment = Button(root, text="Compute Payment", bg="red", fg="white", font="Helvetica 14 bold", command=self.computePayment).grid(row=6, column=2, sticky=E)
 
-        # clear button
+        # Clear button
         btClear = Button(root, text="Clear", bg="blue", fg="white", font="Helvetica 14 bold", command=self.delete_all).grid(row=6, column=8, padx=20, pady=20, sticky=E)
 
         root.mainloop()
 
-    # compute 
+    # Function to compute the payment
     def computePayment(self):
-        # setting parameters for getMontlyPayment function
+        # Calculate the monthly payment using the input values
         montlyPayment = self.getMontlyPayment(
             float(self.loanAmountVar.get()),
             float(self.annualInterestRateVar.get()) / 1200,
             int(self.numberofYearsVar.get()))
 
-        # setting montlyPaymetvar label, result
+        # Update the monthly payment label with the calculated value
         self.monthlyPaymentVar.set(format(montlyPayment, "10.2f"))
 
-        #print(montlyPayment)
-        totalPayment = float(montlyPayment * 12 \
-            * int(self.numberofYearsVar.get()))
+        # Calculate the total payment
+        totalPayment = float(montlyPayment * 12 * int(self.numberofYearsVar.get()))
 
-        # setting totalPaymentVar label, result
+        # Update the total payment label with the calculated value
         self.totalPaymentVar.set(format(totalPayment, "10.2f"))
-    
-    # calculating montly payment
+
+
+    # Function to calculate the monthly payment
     def getMontlyPayment(self, loanAmount, montlyInterestRate, numberofYears):
+        # Calculate the monthly payment using the given formula
         montlyPayment = loanAmount * montlyInterestRate / (1-1/(1 + montlyInterestRate) ** (numberofYears * 12))
         return montlyPayment
 
-    # deleting all information to calculate new
+    # Function to clear all input fields and payment labels
     def delete_all(self):
         self.monthlyPaymentVar.set("")
         self.loanAmountVar.set("")
         self.annualInterestRateVar.set("")
         self.numberofYearsVar.set("")
         self.totalPaymentVar.set("")
-        
 
-# calling class
+# Instantiate the Loan Calculator class
 App()
+
